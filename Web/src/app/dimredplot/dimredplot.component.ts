6d6603773c24bae9b604f50cfe907fc4c1878f80
Tyler Stephenson <tstephenson@focustapps.com>
2022-07-15 12:28:08 -0400

    Transition to google charts and add zoom

--- Web/src/app/dimredplot/dimredplot.component.ts
+++ Web/src/app/dimredplot/dimredplot.component.ts
@@ -9,6 +9,7 @@ import { Options } from '@angular-slider/ngx-slider';
 import { NgxSpinnerService } from "ngx-spinner";
 import { TsneCoordinatesService } from '../services/tsnecoordinates.service';
 import { TsneCoordinate } from '../models/tsneCoordinate';
+import { ChartType } from 'angular-google-charts';
 
 @Component({
   selector: 'app-dimredplot',
@@ -20,20 +21,30 @@ export class DimRedPlotComponent implements OnInit {
   innerWidth: number = window.innerWidth;
 
   view: [number, number] = [this.innerWidth <= 800 ? this.innerWidth - 60 : 1200, 700];
-  bubbleData: any[];
-  tempData: any[];
 
-  // options
-  showXAxis: boolean = true;
-  showYAxis: boolean = true;
-  gradient: boolean = false;
-  showLegend: boolean = false;
-  showXAxisLabel: boolean = false;
-  yAxisLabel: string = 'y';
-  showYAxisLabel: boolean = false;
-  xAxisLabel: string = 'x';
-  maxRadius: number = 1;
-  minRadius: number = 1;
+  // google chart settings
+  chartData: any[] = [];
+  chartType = ChartType.ScatterChart;
+  chartOptions = {
+    legend: 'none',
+    width: 1200,
+    height: 700,
+    title: '',
+    hAxis: { title: '' },
+    vAxis: { title: '' },
+    explorer: {
+      maxZoomIn: 0.05,
+      maxZoomOut: 1.5
+    },
+    pointSize: 4,
+    chartArea: {
+      top: 20,
+      left: 50,
+      width: '90%',
+      height: '90%'
+    }
+  };
+  chartStyle: string = "width: 100%;";
 
   constructor(public spinnerService: NgxSpinnerService, public tsneCoordinateService: TsneCoordinatesService) {
 
@@ -41,27 +52,20 @@ export class DimRedPlotComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.spinnerService.show();
+    this.spinnerService.show();
     this.innerWidth = window.innerWidth;
 
     this.tsneCoordinateService.fetchAll()
       .subscribe(tsneCoordinates => {
 
-        this.tempData = [{
-            name: '',
-            series: []
-        }]
+        this.chartData = [];
 
-        tsneCoordinates.forEach(x => {
-          this.tempData[0].series.push({
-            name: x.X.toString(),
-            x: x.X,
-            y: x.Y,
-            r: 1
-          });
+        tsneCoordinates.forEach(coord => {
+          this.chartData.push([ coord.X, coord.Y ]);
         });
 
-        this.bubbleData = this.tempData;
+        this.spinnerService.hide();
+
       });
   }
 
